8526299aced6088226651dfb42ca66227edc906f
Stuart Geyer <strtg3y3r@gmail.com>
2024-03-09 00:34:13 -0800

    [working] able to deal cards and evaluate one hand; added file "scratch.txt" to put code needed, but cluttering up workspace

--- code/main.py
+++ code/main.py
@@ -12,10 +12,9 @@ valDict = {
 
 altDict = valDict.copy()
 altDict["A"] = 1
-playAgain = ["y", "yes", "yeah", "sure", "Y", "Yes", "Yeah", "Sure", True, "true"]
 
 
-class cardObj:
+class card:
 
     def __init__(self, cardInfo):
         """Reference card suit or val at any point"""
@@ -24,7 +23,7 @@ class cardObj:
         self.val = cardInfo[1]
 
 
-class shoeObj:
+class shoe:
 
     def __init__(self, nDecks):
         """Input a number of decks for the shoe"""
@@ -38,7 +37,7 @@ class shoeObj:
         vals = [i for i in range(2, 11)] + ["J", "Q", "K", "A"]
         cards = list(prod(suits, vals)) * nDecks
         shuffledCards = random.sample(cards, k=len(cards))
-        self.cards = [cardObj(hldr) for hldr in shuffledCards]
+        self.cards = [card(rngCard) for rngCard in shuffledCards]
 
 
     def cutDeck(self):
@@ -46,103 +45,82 @@ class shoeObj:
         self.cut = random.randint(1, 263)
 
 
-class playerObj:
+class player:
 
-    def __init__(self, bankroll, playerNum, bet):
-        """Starting dollar amount"""
-        self.hands = {}
+    def __init__(self, bankroll, playerNum):
+        """Starting dollar amount and player number"""
+        self.hand = {}
         self.name = f"Player{playerNum}"
         self.bankroll = bankroll
-        self.bet = bet
 
 
-class dealerObj:
+class dealer:
     
     def __init__(self):
-        self.hands = {}
+        self.hand = {}
         self.name = "Dealer"
 
 
 class hand:
 
-    def __init__(self, newHand):
+    def __init__(self, nHand, handBet, playerBankroll):
         """Cards that are initially dealt to player"""
-        self.hand = newHand
-        self.evalHand(newHand)
-        pass
-        
+        self.cInfo = [n.info for n in nHand]
+        self.cChar = [n.val for n in nHand]
+        self.cVal = sum(valDict[n.val] for n in nHand)
+        self.Bust = False
+        self.bet = handBet
+        self.BR = playerBankroll
+        self.eval()
+       
 
-    def evalHand(self, hand):
+    def eval(self):
         """Evaluate the player's hand for value and possible moves"""
-        self.options = {}
-        self.options["Hit"] = True
-        self.options["Stand"] = True
-        self.options["Surrender"] = True
-        if len(hand) == 2:
-            # # Split Logic
-            # if hand[0].val == hand[1].val:
-            #     self.options["Split"] = True
-            # else:
-            #     self.options["Split"] = False
-            # # Double-Down Logic
-            # if self.bankroll-self.bet >= self.bet:
-            #     self.options["Double"] = True
-            # else:
-            #     self.options["Double"] = False
-            # # Check for naturals
-            if sum([valDict[hand[0].val], valDict[hand[1].val]]) == 21:
+        self.options = {"Hit": True, "Stand": True, "Surrender": True}
+        self.options["Split"] = False
+        self.options["Double"] = False
+        self.natural = False
+        if len(self.cInfo) == 2:
+            # Check for naturals
+            if self.cVal == 21:
                 self.natural = True
+            # Split
+            if self.cInfo[0][1] == self.cInfo[1][1]:
+                self.options["Split"] = True
+            # Double-Down
+            if self.BR-self.bet >= self.bet:
+                self.options["Double"] = True
+        else:
+            print("we out here")
+            return 0, "0.1.1.3"
+        
+    
+    def addCard(self, nCard):
+        """Added from hitting or doubling-down"""
+        # dict_keys(['info', 'suit', 'val'])
+        self.cInfo += [nCard.info]
+        self.cChar += [nCard.val]
+        self.cVal += valDict[nCard.val]
+        if self.cVal > 21:
+            self.cVal = sum([altDict[i] for i in self.cChar])
+            if self.cVal > 21:
+                self.Bust = True
             else:
-                self.natural = False
-
-
-def checkHand(hand):
-    """Given a hand, this method returns the hand value"""
-    cardSymb = [i[1] for i in hand]  # remove suits for calcs
-    cardVals = [valDict[i] for i in cardSymb]
-    # check for naturals
-    if len(cardSymb) == 2:
-        handVal = sum(cardVals)
-        if handVal == 21:
-            choice = "a.1.0 "
-            return handVal, (choice, "Blackjack!!")
+                self.eval()
         else:
-            choice = "a.2.0"
-            return handVal, (choice, None)
-    elif len(cardSymb) > 2 and "A" not in cardSymb:
-        # No Aces, 3 or more cards in hand
-        handVal = sum(cardVals)
-        if handVal == 21:
-            choice = "b.1.0"
-            return handVal, (choice, "21!")
-        elif handVal < 21:
-            choice = "b.2.0"
-            return handVal, (choice, None)
-        else:
-            choice = "b.3.0"
-            return handVal, (choice, "Bust!")
-    else:
-        # Aces, 3 or more cards in hand
-        handVal = sum(cardVals)
-        if handVal == 21:
-            choice = "c.1.0"
-            return handVal, (choice, "21!")
-        elif  handVal < 21:
-            choice = "c.2.0"
-            return handVal, (choice, None)
-        else:
-            newHandVal = sum([altDict[i] for i in cardSymb])
-            if newHandVal == 21:
-                choice = "c.3.1"
-                return newHandVal, (choice, "21!")
-            elif newHandVal < 21:
-                choice = "c.3.2"
-                return newHandVal, (choice, None)
-            else:
-                choice = "c.3.3"
-                return newHandVal, (choice, "Bust!")
+            self.eval()
 
 
 if __name__ == "__main__":
-    # for now, just 1 playable character and the NPC Dealer
-    players = [playerObj(1, 1000, 50), dealerObj()]
+    # for now, just 1 playable character
+    player = player(1000, 1)
+    shoe = shoe(6)
+
+    mockDeal = [shoe.cards[12], shoe.cards[6]]
+    player.hand[0] = hand(mockDeal, 50, player.bankroll)
+    print(player.hand[0].cInfo)
+    
+    player.hand[0].addCard(shoe.cards[9])
+    print(player.hand[0].cInfo)
+    print(player.hand[0].Bust)
+
